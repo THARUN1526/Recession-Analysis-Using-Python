c56902b2d614be7e79da54a77d889b1194b2642d
THARUN ANAND <103479706+THARUN1526@users.noreply.github.com>
2023-12-16 21:14:46 +0530

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.io as pio
+pio.templates.default = "plotly_white"
+
+data = pd.read_csv('UK_monthly_gdp.csv')
+print(data.head())
